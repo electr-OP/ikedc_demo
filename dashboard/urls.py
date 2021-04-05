c45e8a0ce668bfad056160837430b23cd8f389ec
hayjay <daelectropel@gmail.com>
2021-04-05 12:32:27 +0100

    initial commit

---
+++ dashboard/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'dashboard'
+
+
+urlpatterns = [
+
+
+    # path('login/', views.LoginView.as_view(), name='login'),
+    # path('logout/', views.LogoutView.as_view(), name='logout'),
+    #
+    # path('', views.indexview.as_view(), name='index'),
+
+
+
+
+]
